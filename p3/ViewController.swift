6714b8ddcc6ac56ee4006625599cca3b4a877246
yanyujianshen <ljh274163788@gmail.com>
2020-12-02 23:20:26 -0800

    Initial Commit

---
+++ p3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  p3
+//
+//  Created by Jiahao Lu on 12/2/20.
+//  Copyright © 2020 Jiahao Lu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
