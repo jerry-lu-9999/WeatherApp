a1813b58554034b56ca01fb74429c10a63c2d17a
yanyujianshen <ljh274163788@gmail.com>
2020-12-03 00:12:02 -0800

    first commit

---
+++ p3/TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.swift
+//  p3
+//
+//  Created by Jiahao Lu on 12/2/20.
+//  Copyright © 2020 Jiahao Lu. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
